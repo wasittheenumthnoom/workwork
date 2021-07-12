d7e935fa6e41f6fa42940bf47bca37b3894a3436
wasitthee numthnoom <87303888+wasittheenumthnoom@users.noreply.github.com>
2021-07-12 16:18:40 +0700

    Add files via upload

---
+++ Work1.java
@@ -0,0 +1,17 @@
+
+package work1;
+
+public class Work1 {
+
+    public static void main(String[] args) {
+
+      int sum = 0;
+      int i = 1;
+      for (i = 1; i <= 100; i++) {
+         sum += i;
+      }
+      System.out.println("Summation 1 to 100 = " + sum);
+   }
+}
+    
+
